859f886cb4df014c3250f8b4e280c48f7b897a07
Ben Williams <8enwilliams@gmail.com>
2023-09-27 11:29:04 +1000

    this is great but none of it is needed

--- src/server.zig
+++ src/server.zig
@@ -3,13 +3,18 @@ const mem = std.mem;
 const fs = std.fs;
 const fmt = std.fmt;
 const io = std.io;
+const net = std.net;
+const http = std.http;
 const ArenaAllocator = std.heap.ArenaAllocator;
+
+const log = std.log.scoped(.server);
 
 const FileReader = struct {
     root_path: []const u8,
 
-    // TODO: simplify error signature
-    fn read(self: *FileReader, file_name: []const u8, writer: anytype) error{ ConnectionTimedOut, NetNameDeleted, NotOpenForReading, ConnectionResetByPeer, BrokenPipe, OperationAborted, InputOutput, ReadError, OutOfMemory, SharingViolation, PathAlreadyExists, FileNotFound, AccessDenied, PipeBusy, NameTooLong, InvalidUtf8, BadPathName, Unexpected, NetworkNotFound, InvalidHandle, SymLinkLoop, ProcessFdQuotaExceeded, SystemFdQuotaExceeded, NoDevice, SystemResources, FileTooBig, IsDir, NoSpaceLeft, NotDir, DeviceBusy, FileLocksNotSupported, FileBusy, WouldBlock }!u64 {
+    fn read(self: *FileReader, file_name: []const u8, writer: anytype) !u64 {
+
+        // TODO: we probs don't need an arena here
         var arena = ArenaAllocator.init(std.heap.page_allocator);
         defer arena.deinit();
         const allocator = arena.allocator();
@@ -51,10 +56,94 @@ test "FileReader.read" {
 }
 
 const Server = struct {
-    address: []const u8,
-    port: u16,
+    arena: ArenaAllocator,
+    address: net.Address,
+    server: http.Server,
+    file_reader: FileReader,
 
-    fn start() void {}
+    fn init(address: []const u8, port: u16, acme_challenge_path: []const u8) error{InvalidIPAddressFormat}!Server {
+        var arena = ArenaAllocator.init(std.heap.page_allocator);
+        return Server{
+            .arena = arena,
+            .address = try net.Address.parseIp(address, port),
+            .server = http.Server.init(arena.allocator(), .{}),
+            .file_reader = FileReader{ .root_path = acme_challenge_path },
+        };
+    }
+
+    fn deinit(self: *Server) void {
+        self.server.deinit();
+        self.arena.deinit();
+    }
+
+    // TODO: simplify error signature
+    fn start(self: *Server) !void {
+        var allocator = self.arena.allocator();
+
+        try self.server.listen(self.address);
+
+        outer: while (true) {
+            var response = try self.server.accept(.{
+                .allocator = allocator,
+            });
+
+            defer response.deinit();
+
+            while (response.reset() != .closing) {
+                response.wait() catch |err| switch (err) {
+                    error.HttpHeadersInvalid => continue :outer,
+                    error.EndOfStream => continue,
+                    else => return err,
+                };
+
+                try self.handleRequest(&response);
+            }
+        }
+    }
 
     fn stop() void {}
+
+    fn handleRequest(self: *Server, response: http.Server.Response) !void {
+        log.info("{s} {s} {s}", .{ @tagName(response.request.method), @tagName(response.request.version), response.request.target });
+
+        var allocator = self.arena.allocator();
+        const body = try response.reader().readAllAlloc(allocator, 8192);
+        defer allocator.free(body);
+
+        if (response.request.headers.contains("connection")) {
+            try response.headers.append("connection", "keep-alive");
+        }
+
+        if (!std.mem.startsWith(u8, response.request.target, "/.well-known/acme-challenge")) {
+            response.status = .not_found;
+            try response.do();
+            try response.writeAll("not found\n");
+            try response.finish();
+            return;
+        }
+
+        const file_name_start = std.mem.lastIndexOf(u8, response.request.target, "/");
+        const file_name = response.request.target[file_name_start..];
+
+        const content_length = try self.file_reader.read(file_name, response.writer());
+        response.transfer_encoding = .{ .content_length = content_length };
+
+        try response.headers.append("content-type", "text/plain");
+
+        try response.do();
+        if (response.request.method != .HEAD) {
+            try response.writeAll("Zig ");
+            try response.writeAll("Bits!\n");
+        }
+
+        try response.finish();
+    }
 };
+
+test "Server.start and stop" {
+    var server = try Server.init("127.0.0.1", 1982);
+    defer server.deinit();
+
+    try server.start();
+    //server.stop();
+}
